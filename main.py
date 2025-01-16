bff16e97f22687d407a44673980334422b4934a9
Csilla Toth <csillaout@gmail.com>
2025-01-16 15:06:41 +0000

    encrypt and decrypt failed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 import json
@@ -5,8 +6,6 @@ from pydantic import BaseModel
 from typing import Optional
 from cryptography.fernet import Fernet
 import os
-
-print("cryptography library is installed successfully!")
 
 # Path to store the encryption key
 key_file_path = 'secret.key'
@@ -63,133 +62,82 @@ def read_root():
 
 @app.get("/snippets")
 def get_all_snippets():
-    try:
-        decrypted_snippets = []
-        for snippet in snippets:
-            try:
-                decrypted_snippets.append({**snippet, "code": decrypt_text(snippet["code"])})
-            except Exception as e:
-                print(f"Error decrypting snippet ID {snippet['id']}: {e}")
-                raise HTTPException(status_code=500, detail=f"Error decrypting snippet ID {snippet['id']}")
-        return JSONResponse(content=decrypted_snippets)
-    except Exception as e:
-        print(f"Error decrypting snippets: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error. Please try again later.")
+    # Decrypt all snippets before returning
+    decrypted_snippets = [
+        {**snippet, "code": decrypt_text(snippet["code"])} for snippet in snippets
+    ]
+    return JSONResponse(content=decrypted_snippets)
 
 @app.get("/snippets/{language}")
 def get_snippets_by_language(language: str):
-    try:
-        filtered_snippets = []
-        for snippet in snippets:
-            if snippet["language"].lower() == language.lower():
-                try:
-                    filtered_snippets.append({**snippet, "code": decrypt_text(snippet["code"])})
-                except Exception as e:
-                    print(f"Error decrypting snippet ID {snippet['id']}: {e}")
-                    raise HTTPException(status_code=500, detail=f"Error decrypting snippet ID {snippet['id']}")
-        if not filtered_snippets:
-            raise HTTPException(status_code=404, detail="No snippets found for the specified language")
-        return JSONResponse(content=filtered_snippets)
-    except Exception as e:
-        print(f"Error decrypting snippets: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error. Please try again later.")
+    # Filter snippets by language and decrypt their code
+    filtered_snippets = [
+        {**snippet, "code": decrypt_text(snippet["code"])}
+        for snippet in snippets if snippet["language"].lower() == language.lower()
+    ]
+    if not filtered_snippets:
+        raise HTTPException(status_code=404, detail="No snippets found for the specified language")
+    return JSONResponse(content=filtered_snippets)
 
 @app.get("/snippet/{snippet_id}")
 def get_snippet_by_id(snippet_id: int):
-    try:
-        snippet = next((snippet for snippet in snippets if snippet["id"] == snippet_id), None)
-        if snippet is None:
-            raise HTTPException(status_code=404, detail="Snippet not found")
-        try:
-            decrypted_snippet = {**snippet, "code": decrypt_text(snippet["code"])}
-            return JSONResponse(content=decrypted_snippet)
-        except Exception as e:
-            print(f"Error decrypting snippet ID {snippet['id']}: {e}")
-            raise HTTPException(status_code=500, detail=f"Error decrypting snippet ID {snippet['id']}")
-    except Exception as e:
-        print(f"Error decrypting snippet: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error. Please try again later.")
+    # Find the snippet by its ID and decrypt its code
+    snippet = next((snippet for snippet in snippets if snippet["id"] == snippet_id), None)
+    if snippet is None:
+        raise HTTPException(status_code=404, detail="Snippet not found")
+    decrypted_snippet = {**snippet, "code": decrypt_text(snippet["code"])}
+    return JSONResponse(content=decrypted_snippet)
 
-# function to encrypt text
+# Function to encrypt text
 def encrypt_text(text: str) -> str:
-    try:
-        encrypted_text = cipher_suite.encrypt(text.encode())
-        return encrypted_text.decode()
-    except Exception as e:
-        print(f"Error encrypting text: {e}")
-        raise HTTPException(status_code=500, detail="Error encrypting text.")
+    encrypted_text = cipher_suite.encrypt(text.encode())
+    return encrypted_text.decode()
 
-# function to decrypt text
+# Function to decrypt text
 def decrypt_text(encrypted_text: str) -> str:
-    try:
-        decrypted_text = cipher_suite.decrypt(encrypted_text.encode())
-        return decrypted_text.decode()
-    except Exception as e:
-        print(f"Error decrypting text: {e}")
-        raise HTTPException(status_code=500, detail="Error decrypting text.")
+    decrypted_text = cipher_suite.decrypt(encrypted_text.encode())
+    return decrypted_text.decode()
 
 @app.post("/snippets/", response_model=SnippetWithID)
 def create_snippet(snippet: Snippet):
-    try:
-        # Generate the next available ID
-        next_id = max(snippet["id"] for snippet in snippets) + 1 if snippets else 1
+    next_id = max(snippet["id"] for snippet in snippets) + 1 if snippets else 1
+    encrypted_code = encrypt_text(snippet.code)
+    new_snippet = {"id": next_id, "language": snippet.language, "code": encrypted_code}
+    snippets.append(new_snippet)
+    
+    with open(file_path, 'w') as file:
+        json.dump(snippets, file, indent=4)
 
-        # encrypt the snippet's code
-        encrypted_code = encrypt_text(snippet.code)
-
-        new_snippet = {"id": next_id,  "language": snippet.language, 'code': encrypted_code}
-        snippets.append(new_snippet)
-        
-        # Save the newly created snippet to the file
-        with open(file_path, 'w') as file:
-            json.dump(snippets, file, indent=4)
-
-        return new_snippet
-    except Exception as e:
-        print(f"Error creating snippet: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error. Please try again later.")
+    return new_snippet
 
 @app.delete("/snippets/{language}")
 def delete_snippets_by_language(language: str):
     global snippets
-    try:
-        # filter snippets by language and delete them 
-        snippets_to_delete = [snippet for snippet in snippets if snippet["language"].lower() == language.lower()]
-        if not snippets_to_delete:
-            raise HTTPException(status_code=404, detail="No snippet found")
-        snippets = [snippet for snippet in snippets if snippet["language"].lower() != language.lower()]
+    snippets_to_delete = [snippet for snippet in snippets if snippet["language"].lower() == language.lower()]
+    if not snippets_to_delete:
+        raise HTTPException(status_code=404, detail="No snippet found")
+    snippets = [snippet for snippet in snippets if snippet["language"].lower() != language.lower()]
 
-        # Save the updated snippets to the file
-        with open(file_path, 'w') as file:
-            json.dump(snippets, file, indent=4)
-        
-        return snippets_to_delete
-    except Exception as e:
-        print(f"Error deleting snippets: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error. Please try again later.")
+    with open(file_path, 'w') as file:
+        json.dump(snippets, file, indent=4)
+    
+    return snippets_to_delete
 
 @app.put("/snippet/{snippet_id}", response_model=SnippetWithID)
 def update_snippet(snippet_id: int, snippet_update: UpdateSnippet):
-    try:
-        # Find the snippet by its ID
-        snippet = next((snippet for snippet in snippets if snippet["id"] == snippet_id), None)
-        if snippet is None:
-            raise HTTPException(status_code=404, detail="Snippet not found")
+    snippet = next((snippet for snippet in snippets if snippet["id"] == snippet_id), None)
+    if snippet is None:
+        raise HTTPException(status_code=404, detail="Snippet not found")
 
-        # Update the snippet details
-        if snippet_update.language is not None:
-            snippet["language"] = snippet_update.language
-        if snippet_update.code is not None:
-            snippet["code"] = encrypt_text(snippet_update.code)
-        
-        # Save the updated snippets to the file
-        with open(file_path, 'w') as file:
-            json.dump(snippets, file, indent=4)
-        
-        return snippet
-    except Exception as e:
-        print(f"Error updating snippet: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error. Please try again later.")
+    if snippet_update.language is not None:
+        snippet["language"] = snippet_update.language
+    if snippet_update.code is not None:
+        snippet["code"] = encrypt_text(snippet_update.code)
+    
+    with open(file_path, 'w') as file:
+        json.dump(snippets, file, indent=4)
+    
+    return snippet
 
 # Run the application
 if __name__ == "__main__":
